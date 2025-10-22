2cc33b48a21154fe9771723c9c2e95f6edbcdb9e
Wang-Huiqi <2632940400@qq.com>
2025-10-22 10:44:28 +0800

    add

---
+++ 78.子集.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=cpp
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> path;
+        auto dfs = [&](this auto&& dfs, int i)->void {
+            if (i == n) {
+                ans.push_back(path);
+                return;
+            }
+            dfs(i + 1);
+            path.push_back(nums[i]);
+            dfs(i + 1);
+            path.pop_back();
+        };
+        dfs(0);
+        return ans;
+    }
+};
+// @lc code=end
+
